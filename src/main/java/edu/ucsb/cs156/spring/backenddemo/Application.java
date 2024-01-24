e51d668447b98a9934b0b47d4e4b465e93f97561
Bharat Kathi <bharat1031@gmail.com>
2024-01-23 19:08:02 -0800

    added service tests

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/Application.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/Application.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs156.spring.backenddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
