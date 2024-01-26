5ab3ed0c7bed7f2b9baf5aa7cd4893ae4c8056eb
AlexLim-Pro <alexlim@umail.ucsb.edu>
2024-01-25 17:57:50 -0800

    updating controllerr

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
@@ -2,7 +2,35 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.ucsb.cs156.spring.backenddemo.services.LocationQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
 @RestController
+@RequestMapping("/api/location")
 public class LocationController {
     
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    LocationQueryService locationQueryService;
+
+    @GetMapping("/get")
+    public ResponseEntity<String> getLocation() throws JsonProcessingException {
+        String result = locationQueryService.getJSON();
+        return ResponseEntity.ok().body(result);
+
+
+        
+    }
 }
