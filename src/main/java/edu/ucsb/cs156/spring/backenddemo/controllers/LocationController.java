65886593e182cbf12181debff58350672f3049e8
Bharat Kathi <bharat1031@gmail.com>
2024-01-31 12:32:32 -0800

    Update LocationController.java

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
@@ -18,6 +18,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name="Location Info from openstreetmap")
 @Slf4j
 @RestController
 @RequestMapping("/api/location")
