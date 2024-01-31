9efd2b8c36ce26c68797462930c936bdc5e707b7
Alex Lim <alex@virtueware.com>
2024-01-30 17:32:18 -0800

    updating location query services and controller tests

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
@@ -1,7 +1,5 @@
 package edu.ucsb.cs156.spring.backenddemo.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,17 +8,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import edu.ucsb.cs156.spring.backenddemo.services.LocationQueryService;
-import edu.ucsb.cs156.spring.backenddemo.services.PublicHolidayQueryService;
 
-
-import static org.mockito.ArgumentMatchers.eq;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
 
 
 @WebMvcTest(value = LocationController.class)
--- src/test/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryServiceTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryServiceTests.java
@@ -2,11 +2,8 @@ package edu.ucsb.cs156.spring.backenddemo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
-
-import edu.ucsb.cs156.spring.backenddemo.beans.CollegeSubreddit;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,54 +17,32 @@ import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RestClientTest(CollegeSubredditQueryService.class)
-
-public class CollegeSubredditQueryServiceTests {
+@RestClientTest(LocationQueryService.class)
+public class LocationQueryServiceTests {
 
     @Autowired
     private MockRestServiceServer mockRestServiceServer;
 
     @Autowired
-    private CollegeSubredditQueryService collegeSubredditQueryService;
+    private LocationQueryService locationQueryService;
 
     @Test
     public void test_getLocations() throws URISyntaxException, UnsupportedEncodingException, JsonProcessingException {
+        String location = "University of California - Santa Barbara";
+        String expectedURL = LocationQueryService.ENDPOINT.replace("{location}", URLEncoder.encode(location, StandardCharsets.UTF_8.toString()));
 
-        String expectedURL = CollegeSubredditQueryService.ENDPOINT;
-        String fakeCSVData = "name,location,subreddit\n"
-                + "\"University of California - Santa Barbara\",\"Santa Barbara, California, United States\",UCSantaBarbara\n"
-                + "\"University of California - Santa Cruz\",\"Santa Cruz, California, United States\",UCSC";
+        String fakeJsonString = "{ \"fake\": \"data\" }";
 
-        List<CollegeSubreddit> subreddits = new ArrayList<CollegeSubreddit>();
-        CollegeSubreddit ucsb = new CollegeSubreddit();
-        ucsb.setName("University of California - Santa Barbara");
-        ucsb.setLocation("Santa Barbara, California, United States");
-        ucsb.setSubreddit("UCSantaBarbara");
-        CollegeSubreddit ucsc = new CollegeSubreddit();
-        ucsc.setName("University of California - Santa Cruz");
-        ucsc.setLocation("Santa Cruz, California, United States");
-        ucsc.setSubreddit("UCSC");
-        subreddits.add(ucsb);
-        subreddits.add(ucsc);
-
-        ObjectMapper mapper = new ObjectMapper();
-        String expectedJSON = mapper.writeValueAsString(subreddits);
-
-        java.net.URI uri = new java.net.URI(expectedURL);
-
-        this.mockRestServiceServer.expect(requestTo(uri))
+        this.mockRestServiceServer.expect(requestTo(expectedURL))
                 .andExpect(header("Accept", "text/plain"))
                 .andExpect(header("Content-Type", MediaType.APPLICATION_JSON.toString()))
-                .andRespond(withSuccess(fakeCSVData, MediaType.TEXT_PLAIN));
+                .andRespond(withSuccess(fakeJsonString, MediaType.TEXT_PLAIN));
 
-        String actualResult = collegeSubredditQueryService.getJSON();
-        assertEquals(expectedJSON, actualResult);
+        String actualResult = locationQueryService.getJSON();
+        assertEquals(fakeJsonString, actualResult);
     }
 
 }
