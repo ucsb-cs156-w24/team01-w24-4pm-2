b4e9496a6f6a598307b3fb6d9ae509123522aa98
Bharat Kathi <bharat1031@gmail.com>
2024-01-31 12:34:31 -0800

    Update LocationController.java

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
@@ -3,6 +3,7 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 import edu.ucsb.cs156.spring.backenddemo.services.LocationQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
