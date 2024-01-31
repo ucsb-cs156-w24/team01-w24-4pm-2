d3a84ad48ccaf9c3f34f5a8f4c6034745bc2a6fd
Kyle Kumar <kylebkumar@gmail.com>
2024-01-30 17:55:22 -0800

    removed unused import

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
@@ -2,7 +2,6 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import edu.ucsb.cs156.spring.backenddemo.services.EarthquakeQueryService;
 import edu.ucsb.cs156.spring.backenddemo.services.UniversityQueryService;
 import lombok.extern.slf4j.Slf4j;
 
